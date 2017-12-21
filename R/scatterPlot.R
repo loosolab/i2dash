26c04f50a00f8537e8c2c42d7a2eb3e35f7155aa
Schultheis <hschult@kerckhoff.mpg.de>
2017-12-21 13:26:42 +0100

    scatterPlot: use external css file

--- R/scatterPlot.R
+++ R/scatterPlot.R
@@ -9,6 +9,7 @@ scatterPlotUI <- function(id) {
   ns <- shiny::NS(id)
 
   shiny::tagList(shiny::fluidPage(
+    shiny::singleton(shiny::tags$head(shiny::tags$link(rel = "stylesheet", type = "text/css", href = "wilson_www/styles.css"))),
     shiny::fluidRow(shinydashboard::box(
       width = 12,
       shiny::div(style = "overflow-y: scroll; overflow-x: scroll; height: 800px; text-align: center",
@@ -271,10 +272,12 @@ scatterPlot <- function(input, output, session, data, types, features = NULL, ma
           result$highlight.labels <- markerReac()$label
         } else {
           shiny::showNotification(
+            id = session$ns("label-limit"),
             paste("Warning! Label restricted to 100 or less labels. Currently selected:", length(markerReac()$label)),
-            duration = 10,
+            duration = NULL,
             type = "warning"
           )
+          shinyjs::addClass(selector = paste0("#shiny-notification-", session$ns("label-limit")), class = "notification-position-center")
         }
       }
 
