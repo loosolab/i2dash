70bfa601516cf8a706b4935d670f3092f0634f4c
Schultheis <hschult@kerckhoff.mpg.de>
2018-05-07 12:30:50 +0200

    featureSelector: use clarion object

--- exec/featureSelector_example.R
+++ exec/featureSelector_example.R
@@ -6,16 +6,17 @@ source("../R/orTextual.R")
 source("../R/featureSelector.R")
 source("../R/function.R")
 source("../R/global.R")
+source("../R/clarion.R")
 
 # test data
 data <- data.table::as.data.table(mtcars, keep.rowname = "id")
 # create metadata
-metadata <- data.table::data.table(names(data), type = c("annotation", rep("performance", 7), rep("design", 4)))
+metadata <- data.table::data.table(names(data), level = c("feature", rep("sample", 7), rep("condition", 4)))
 names(metadata)[1] <- "key"
+clarion <- Clarion$new(data = data, metadata = metadata)
 
 ui <- dashboardPage(header = dashboardHeader(),
                     sidebar = dashboardSidebar(
-                      selectInput("columns", label = "Features to select from", choices = names(data), multiple = TRUE),
                       verbatimTextOutput("filter")
 ), dashboardBody(fluidPage(
   featureSelectorUI(id = "id")
@@ -24,10 +25,16 @@ ui <- dashboardPage(header = dashboardHeader(),
 
 server <- function(input, output) {
 
-  mod <-callModule(featureSelector, "id", data = data, delimiter = ",", features = reactive(input$columns), feature.grouping = metadata)
+  mod <- callModule(featureSelector, "id", clarion = clarion)
 
   output$filter <- renderText({
     paste(mod()$filter, collapse = "\n")
+  })
+
+  observe({
+    print(mod()$object$header)
+    print(mod()$object$metadata)
+    print(mod()$object$data)
   })
 
 }
