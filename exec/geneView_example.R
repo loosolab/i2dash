a72e328fc98adb5b36ccf479baf866c954b40251
Schultheis <hschult@kerckhoff.mpg.de>
2018-05-09 08:55:19 +0200

    geneview_example: added factors

--- exec/geneView_example.R
+++ exec/geneView_example.R
@@ -14,7 +14,7 @@ source("../R/clarion.R")
 # test data
 data <- data.table::as.data.table(mtcars, keep.rowname = "id")
 # create metadata
-metadata <- data.table::data.table(names(data), level = c("feature", rep("sample", 7), rep("condition", 4)))
+metadata <- data.table::data.table(names(data), level = c("feature", rep("sample", 7), rep("condition", 4)), factor1 = c(rep("a", 5), rep("b", 7)), factor2 = c(rep("1", 7), rep("2", 5)), factor3 = c(rep("", 4), rep("test", 5), rep("test2", 3)))
 names(metadata)[1] <- "key"
 clarion <- Clarion$new(data = data, metadata = metadata)
 ####
