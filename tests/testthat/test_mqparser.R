ec3d416a917087f03ff975634577c5c67b1c7e01
renewiegandt <rene.wiegandt@mpi-bn.mpg.de>
2018-07-13 09:43:03 +0200

    removed redundant :: notation

--- tests/testthat/test_mqparser.R
+++ tests/testthat/test_mqparser.R
@@ -1,15 +1,15 @@
 context("MaxQuant parser")
 
-testthat::test_that("all needed input parameteres are given", {
+test_that("all needed input parameteres are given", {
   expect_error(wilson::parse_MaxQuant(),"The proteinGroups file was not given")
-  expect_error(wilson::parse_MaxQuant(proteinGroups_in = "/path/path/"),"The summary file was not given")
-  expect_error(wilson::parse_MaxQuant(proteinGroups_in = "/path/path/",summary_in = "/path/path/"),
+  expect_error(wilson::parse_MaxQuant(proteinGroups_in = "/path/path/"), "The summary file was not given")
+  expect_error(wilson::parse_MaxQuant(proteinGroups_in = "/path/path/", summary_in = "/path/path/"),
                                       "The output file was not given")
-  expect_error(wilson::parse_MaxQuant(proteinGroups_in = "/path/path/",summary_in = "/path/path/",
-                                      outfile = "/path/path/"),"The output_reduced file was not given")
+  expect_error(wilson::parse_MaxQuant(proteinGroups_in = "/path/path/", summary_in = "/path/path/",
+                                      outfile = "/path/path/"), "The output_reduced file was not given")
 })
 
-testthat::test_that("mq_parser",{
+test_that("mq_parser",{
 
   expect_error(wilson::parse_MaxQuant(proteinGroups_in = system.file("/tests/testthat", "proteinGroups_test.txt", package = "wilson"),
                                       summary_in = system.file("/tests/testthat", "summary_test_2.txt", package = "wilson"),
