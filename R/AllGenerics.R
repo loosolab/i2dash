e9dda622565c31e41f32d38fbb27393bc843bf45
Jens Preussner <jens.preussner@mpi-bn.mpg.de>
2019-07-10 11:35:58 +0200

    Fixed file order for proper install

--- R/AllGenerics.R
+++ R/AllGenerics.R
@@ -1,5 +1,11 @@
-# Generics
+#' @export
 setGeneric("assemble", function(object, ...) standardGeneric("assemble"))
+
+#' @export
 setGeneric("add_page", function(object, ...) standardGeneric("add_page"))
+
+#' @export
 setGeneric("remove_page", function(object, ...) standardGeneric("remove_page"))
+
+#' @export
 setGeneric("add_component", function(object, component, ...) standardGeneric("add_component"))
