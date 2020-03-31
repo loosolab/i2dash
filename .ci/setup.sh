e35aafdc68236b05980e826ccc3631a858a8a543
Schultheis <hschult@kerckhoff.mpg.de>
2020-03-31 14:35:50 +0200

    update everything

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -8,7 +8,7 @@ Rscript -e 'install.packages("BiocManager", repos="http://cran.r-project.org")'
 # https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
 while IFS= read -r package;
 do
-  Rscript -e "BiocManager::install('"$package"')";
+  Rscript -e "BiocManager::install('"$package"', update = TRUE, ask = FALSE)";
 done < ".ci/r-requirements.txt"
 
 # install other dependencies
