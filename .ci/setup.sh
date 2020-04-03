4e8559bd9c12d145e3667585afbd881b288c2c2f
Schultheis <hschult@kerckhoff.mpg.de>
2020-04-03 13:16:24 +0200

    actually use r-devel

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -13,11 +13,26 @@ do
 done < ".ci/apt-requirements.txt"
 
 ##### R #####
-# install BiocManager
-Rscript -e 'install.packages("BiocManager", repos="http://cran.r-project.org")'
-# install r dependencies
-# https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
-while IFS= read -r package;
-do
-  Rscript -e "BiocManager::install('"$package"', update = TRUE, ask = FALSE)";
-done < ".ci/r-requirements.txt"
+# check if Rdevel is available
+if test -z $(which RD)
+then
+  # install BiocManager
+  Rscript -e 'install.packages("BiocManager", repos="http://cran.r-project.org")'
+  # install r dependencies
+  # https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
+  while IFS= read -r package;
+  do
+    Rscript -e "BiocManager::install('"$package"', update = TRUE, ask = FALSE)";
+  done < ".ci/r-requirements.txt"
+else
+  # install BiocManager
+  RDscript -e 'install.packages("BiocManager", repos="http://cran.r-project.org")'
+  # update BiocManager to devel
+  RDscript -e 'BiocManager::install(version = "devel", update = TRUE, ask = FALSE)'
+  # install r dependencies
+  # https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
+  while IFS= read -r package;
+  do
+    RDscript -e "BiocManager::install('"$package"', update = TRUE, ask = FALSE)";
+  done < ".ci/r-requirements.txt"
+fi
