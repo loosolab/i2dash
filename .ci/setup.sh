1aa7ca84f69ee5217549f3176f52a5f8474cee6f
Schultheis <hschult@kerckhoff.mpg.de>
2020-04-03 17:57:58 +0200

    Fix r-devel wrong package version error

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -29,6 +29,8 @@ else
   RDscript -e 'install.packages("BiocManager", repos="http://cran.r-project.org")'
   # update BiocManager to devel
   RDscript -e 'BiocManager::install(version = "devel", update = TRUE, ask = FALSE)'
+  # reinstall all packages to avoid "Error: package X was installed before R 4.0.0: please re-install it"
+  RDscript -e 'BiocManager::install(installed.packages()[,1])'
   # install r dependencies
   # https://www.cyberciti.biz/faq/unix-howto-read-line-by-line-from-file/
   while IFS= read -r package;
